5586d499936d5a2444782c78b427523435fb961d
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-07-16 13:53:46 +0900

    feat : Basic multiplayer added

--- Assets/Script/Pyun/Character2DController.cs
+++ Assets/Script/Pyun/Character2DController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class Character2DController : MonoBehaviour
 {
@@ -10,6 +12,7 @@ public class Character2DController : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private SpriteRenderer spriteRenderer;
     Animator anim;
+    PhotonView view;
 
     // Start is called before the first frame update
     void Start()
@@ -17,60 +20,65 @@ public class Character2DController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Move Speed
-        float h = Input.GetAxisRaw("Horizontal");
-        _rigidbody.AddForce(Vector2.right*h, ForceMode2D.Impulse);
-
-        //Max Speed
-        if(_rigidbody.velocity.x > maxSpeed)
-            _rigidbody.velocity = new Vector2(maxSpeed, _rigidbody.velocity.y);
-        else if(_rigidbody.velocity.x < maxSpeed*(-1))
-            _rigidbody.velocity = new Vector2(maxSpeed*(-1), _rigidbody.velocity.y);
-
-
-        //Stop Speed
-        if(Input.GetButtonUp("Horizontal")){
-            _rigidbody.velocity = new Vector2(0.5f*_rigidbody.velocity.normalized.x, _rigidbody.velocity.y);
-        }
-
-        // if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
-        // {
-        //     _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        // }
-        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        if(view.IsMine)
         {
-            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            anim.SetBool("isJumping", true);
-        }
-        //Landing platform
-        if (_rigidbody.velocity.y < 0)
-            {
-            Debug.DrawRay(_rigidbody.position, Vector3.down, new Color(0, 1, 0));
+            //Move Speed
+            float h = Input.GetAxisRaw("Horizontal");
+            _rigidbody.AddForce(Vector2.right*h, ForceMode2D.Impulse);
 
-            RaycastHit2D rayHit = Physics2D.Raycast(_rigidbody.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            //Max Speed
+            if(_rigidbody.velocity.x > maxSpeed)
+                _rigidbody.velocity = new Vector2(maxSpeed, _rigidbody.velocity.y);
+            else if(_rigidbody.velocity.x < maxSpeed*(-1))
+                _rigidbody.velocity = new Vector2(maxSpeed*(-1), _rigidbody.velocity.y);
 
-            if(rayHit.collider != null)
-            {
-                if(rayHit.distance < 0.5f)
-                    anim.SetBool("isJumping", false);
+
+            //Stop Speed
+            if(Input.GetButtonUp("Horizontal")){
+                _rigidbody.velocity = new Vector2(0.5f*_rigidbody.velocity.normalized.x, _rigidbody.velocity.y);
             }
 
+            // if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+            // {
+            //     _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            // }
+            if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+            {
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                anim.SetBool("isJumping", true);
+            }
+            //Landing platform
+            if (_rigidbody.velocity.y < 0)
+            {
+                Debug.DrawRay(_rigidbody.position, Vector3.down, new Color(0, 1, 0));
+
+                RaycastHit2D rayHit = Physics2D.Raycast(_rigidbody.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+                if(rayHit.collider != null)
+                {
+                    if(rayHit.distance < 0.5f)
+                        anim.SetBool("isJumping", false);
+                }
+
+            }
+
+
+            //Direction Sprite
+            if (Input.GetButtonDown("Horizontal"))
+                spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+            //Animation
+            if (Mathf.Abs(_rigidbody.velocity.x) < 0.3)
+                anim.SetBool("isWalking", false);
+            else
+                anim.SetBool("isWalking", true);
         }
-
-
-        //Direction Sprite
-        if (Input.GetButtonDown("Horizontal"))
-            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-
-        //Animation
-        if (Mathf.Abs(_rigidbody.velocity.x) < 0.3)
-            anim.SetBool("isWalking", false);
-        else
-            anim.SetBool("isWalking", true);
     }
+        
 }
