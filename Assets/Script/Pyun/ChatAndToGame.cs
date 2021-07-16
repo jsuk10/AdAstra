25339444008ee7eb06a534728f11c608804c5159
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-07-16 20:37:18 +0900

    feat : Waiting room with chat added

--- Assets/Script/Pyun/ChatAndToGame.cs
+++ Assets/Script/Pyun/ChatAndToGame.cs
@@ -10,10 +10,14 @@ public class ChatAndToGame : MonoBehaviourPunCallbacks
 {
     public Text ListText;
     public Text[] ChatText;
+    public PhotonView PV;
+    public InputField ChatInput;
     
     void Start()
     {
         RoomRenewal();
+        ChatInput.text = "";
+        for(int i = 0; i < ChatText.Length; i++) ChatText[i].text = "";
     }
 
     void Update()
@@ -21,25 +25,14 @@ public class ChatAndToGame : MonoBehaviourPunCallbacks
         
     }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer) 
+    public void LeaveRoom()
     {
-        RoomRenewal();
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("Lobby");
     }
-
-    public override void OnPlayerLeftRoom(Player otherPlayer) 
-    {
-        RoomRenewal();
-    }
-
-    public void RoomRenewal()
-    {
-        ListText.text = "";
-        for(int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
-            ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i+1  == PhotonNetwork.PlayerList.Length)?"":", ");
-    }
-
+    
     [PunRPC] //플레이어 속해있는방 모든 인원에게 전함
-    void ChatPRC(string msg)
+    void ChatRPC(string msg)
     {
         bool isInput = false;
         for (int i = 0; i < ChatText.Length; i++)
@@ -54,5 +47,43 @@ public class ChatAndToGame : MonoBehaviourPunCallbacks
             for(int i = 1; i < ChatText.Length; i++) ChatText[i-1].text = ChatText[i].text;
             ChatText[ChatText.Length-1].text = msg;
         }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer) 
+    {
+        RoomRenewal();
+        ChatRPC("<color=yellow>" + newPlayer.NickName + "님이 참가하셨습니다</color>");
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer) 
+    {
+        RoomRenewal();
+        ChatRPC("<color=yellow>" + otherPlayer.NickName + "님이 퇴장하셨습니다</color>");
+    }
+
+    public void RoomRenewal()
+    {
+        ListText.text = "";
+        for(int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i+1  == PhotonNetwork.PlayerList.Length)?"":", ");
+    }
+
+    public void Send()
+    {
+        PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName+ " : "+ ChatInput.text);
+        Debug.Log(PhotonNetwork.NickName+ " : "+ ChatInput.text);
+        ChatInput.text = "";
+    }
+
+
+    [PunRPC]
+    void GameRPC()
+    {
+        PhotonNetwork.LoadLevel("PyunScene");
+    }
+
+    public void clickToGame()
+    {
+        PV.RPC("GameRPC", RpcTarget.All);
     }
 }
