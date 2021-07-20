9ad776131490b0520c5bff3cb170f921c90ac7b5
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-07-21 00:21:42 +0900

    chore : Minor edits

---
+++ Assets/elonSound2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elonSound2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.Instance.SFXPlayer("endingElon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
